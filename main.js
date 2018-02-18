92d5c53d958c3e14b38f7c9e1e6acea0b7e93007
KasperMin <kaspermsimonsen@gmail.comgit config --global user.email git config --global user.email kaspermsimonsen@gmail.com>
2018-02-18 14:07:31 +0100

    first counter finished

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
+// - If you are at the footer When the first image switch happens it automatically jump up to the head //
+$(document).ready(function() {
+  let cambodiaCounterEl = $("#cambodiaCounter");
+  let documentEl = $(document);
+  let counter = 0
+  let scrollActivated = false;
+  let breakActivated = false;
+  let interval;
 
-let imageSources = ["./assets/sad_girl.jpg", "./assets/fundraising_image.jpg" ];
-let index = 0;
+  documentEl.on("scroll", function() {
+    if (documentEl.scrollTop() > 100 & scrollActivated === false) {
+      scrollActivated = true;
 
-setInterval (function() {
-  if (index === imageSources.length) {
-    index = 0;
-  }
-  document.getElementById("slideImage").src = imageSources[index];
-  index++;
-}, 5000);
+      function startCounter() {
+        cambodiaCounterEl.html(counter);
+        if (counter < 15000) {
+          counter++;
+        } else {
+          clearInterval(interval)
+        }
+      } // END OF START COUNTER FUNCTION //
+
+      for (let i = 0; i < 100; i++) {
+        interval = setInterval(startCounter, 1)
+      }
+
+
+     }; // END OF IF SCROLL STATEMENT //
+
+  }); // END OF SCROLL FUNCITON //
+
+}); // End of ready function //
